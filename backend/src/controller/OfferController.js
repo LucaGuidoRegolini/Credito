6d5b470680f41172a0f00194596e96c15d238ea6
Luca Guido Regolini <70727804+LucaGuidoRegolini@users.noreply.github.com>
2021-06-22 18:38:07 -0300

    finalização do backend

--- backend/src/controller/OfferController.js
+++ backend/src/controller/OfferController.js
@@ -6,73 +6,126 @@ module.exports = {
       const offers = await connection("credit_offers");
       if (offers.length == 0) {
         return res.status(500).json({
-        sucess: false,
-        msg: "offers not found",
-      })
+          sucess: false,
+          msg: "offers not found",
+        });
       }
       return res.status(200).json({
         sucess: true,
         msg: "offers found",
-        offers
-      })
-
+        offers,
+      });
     } catch (error) {
       return res.status(400).json({
         sucess: false,
-         msg: "offers erro",
+        msg: "offers erro",
         error: `>Erro: ${error}`,
-      })
+      });
     }
   },
 
   async search(req, res) {
     const { id } = req.params;
     try {
-      const offers = await connection("credit_offers")
-        .where('min_score', '>=', id);
+      const offers = await connection("credit_offers").where(
+        "min_score",
+        ">=",
+        id
+      );
       return res.status(200).json({
         sucess: true,
         msg: "offers found",
-        offers
-      })
+        offers,
+      });
     } catch (error) {
       return res.status(400).json({
         sucess: false,
-         msg: "offers erro",
+        msg: "offers erro",
         error: `>Erro: ${error}`,
-      })
+      });
+    }
+  },
+
+  async select(req, res) {
+    const { id } = req.params;
+    try {
+      const offer = await connection("credit_offers").where("id", id);
+      if (offer.length == 0) {
+        return res.status(500).json({
+          sucess: false,
+          msg: "offer not found",
+        });
+      }
+      return res.status(200).json({
+        sucess: true,
+        msg: "offer found",
+        offer,
+      });
+    } catch (error) {
+      return res.status(400).json({
+        sucess: false,
+        msg: "offers erro",
+        error: `>Erro: ${error}`,
+      });
     }
   },
 
   async create(req, res) {
     const {
       min_score,
-      max_loan ,
+      max_loan,
       interest_rate,
-      financial_agency = "" } = req.body
+      financial_agency = "",
+    } = req.body;
     try {
       await connection("credit_offers").insert({
         min_score,
         max_loan,
         interest_rate,
-        financial_agency
-      })
+        financial_agency,
+      });
       return res.status(200).json({
         sucess: true,
         msg: "offer created",
         offer: {
           min_score,
-        max_loan,
-        interest_rate,
-        financial_agency
-        }
-      })
+          max_loan,
+          interest_rate,
+          financial_agency,
+        },
+      });
     } catch (error) {
       return res.status(400).json({
         sucess: false,
-         msg: "offers erro",
+        msg: "offers erro",
         error: `>Erro: ${error}`,
-      })
+      });
     }
-  }
+  },
+
+  async delete(req, res) {
+    const { id } = req.params;
+    try {
+      const offer = await connection("credit_offers").where("id", id);
+      if (offer.length == 0) {
+        return res.status(500).json({
+          sucess: false,
+          msg: "offer not found",
+        });
+      }
+
+      await connection("credit_offers").where("id", id).delete();
+      return res.status(200).json({
+        sucess: true,
+        msg: "offer deleted",
+        offer,
+      });
+    } catch (error) {
+      return res.status(400).json({
+        sucess: false,
+        msg: "offers erro",
+        error: `>Erro: ${error}`,
+      });
+    }
+  },
 };
--- backend/src/router.js
+++ backend/src/router.js
@@ -5,9 +5,11 @@ const validation = require("./validation");
 const OfferController = require("./controller/OfferController");
 const SaleController = require("./controller/SaleController");
 
-router.get("/offer", OfferController.index);
-router.get("/offer/:id", OfferController.search);
+router.get("/offers", OfferController.index);
+router.get("/offers/:id", OfferController.search);
+router.get("/offer/:id", OfferController.select);
 router.post("/offer", OfferController.create);
+router.delete("/delete/offer/:id", OfferController.delete);
 
 router.get("/sale", SaleController.index);
 router.get("/sale/:id", SaleController.select);
